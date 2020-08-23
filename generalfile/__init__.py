4e86259bfe9dab8b6b6fd3868fdd0a5cb4e819b7
Mandera <krizoorn@hotmail.com>
2020-08-23 22:09:53 +0200

    Initial commit

--- generalfile/__init__.py
+++ generalfile/__init__.py
@@ -1,5 +1,3 @@
 
 from generalfile.path import Path
 from generalfile.errors import *
-
-Path.get_lock_dir().create_folder()
--- generalfile/path.py
+++ generalfile/path.py
@@ -1,114 +1,55 @@
 
 import pathlib
 
-
-from generallibrary import VerInfo, initBases
-
+from generallibrary import VerInfo
 from generalfile.errors import *
-from generalfile.path_lock import Path_ContextManager
-from generalfile.path_operations import Path_Operations
-from generalfile.path_strings import Path_Strings
-from generalfile.optional_dependencies.path_spreadsheet import Path_Spreadsheet
 
 
-@initBases
-class Path(Path_ContextManager, Path_Operations, Path_Strings, Path_Spreadsheet):
+class Path:
     """
-    Immutable cross-platform Path.
-    Wrapper for pathlib.
-    Implements rules to ensure cross-platform compatability.
+    Layer on top of pathlib's Path.
+    Implements rules to ensure cross-platform compatabilities.
     Adds useful methods.
     """
-    verInfo = VerInfo()
-    path_delimiter = verInfo.pathDelimiter
-    Path = ...
+    suffixDelimiter = "_"
+    pathDelimiter = VerInfo().pathDelimiter
+    _suffixIO = {"txt": ("txt", "md", ""), "tsv": ("tsv", "csv")}
 
-    def __init__(self, path=None):
-        self._str_path = self._scrub(str_path="" if path is None else str(path))
-        self._path = pathlib.Path(self._str_path)
+    def _replaceDelimiters(self, strPath):
+        strPath = strPath.replace("/", self.pathDelimiter)
+        strPath = strPath.replace("\\", self.pathDelimiter)
+        return strPath
 
-    def _scrub(self, str_path):
-        str_path = self._replace_delimiters(str_path=str_path)
-        str_path = self._invalid_characters(str_path=str_path)
-        str_path = self._trim(str_path=str_path)
-        return str_path
+    def __init__(self, path):
+        verInfo = VerInfo()
+        strPath = "" if path is None else str(path)
 
-    def _replace_delimiters(self, str_path):
-        str_path = str_path.replace("/", self.path_delimiter)
-        str_path = str_path.replace("\\", self.path_delimiter)
-        # str_path = str_path.replace(self.path_delimiter_alternative, self.path_delimiter)  # Don't remember why I commented this
-        return str_path
+        strPath = self._replaceDelimiters(strPath=strPath)
 
-    def _invalid_characters(self, str_path):
+
+        self._path = pathlib.Path(strPath)
+
+
+
+
+
+
         # Simple invalid characters testing from Windows
         for character in '<>"|?*':
-            if character in str_path:
-                raise InvalidCharacterError(f"Invalid character '{character}' in '{str_path}'")
-        if ":" in str_path:
-            if not self.verInfo.pathRootHasColon:
-                raise InvalidCharacterError(f"Path has a colon but '{self.verInfo.os}' doesn't use colon for path root: '{str_path}'")
-            if str_path[1] != ":":
-                raise InvalidCharacterError(f"Path has a colon but there's no colon at index 1: '{str_path}'")
-            if len(str_path) >= 3 and str_path[2] != self.path_delimiter:
-                raise InvalidCharacterError(f"Path has a colon but index 2 is not a delimiter: '{str_path}'")
-            if ":" in str_path[2:]:
-                raise InvalidCharacterError(f"Path has a colon that's not at index 1: '{str_path}'")
-        if str_path.endswith("."):
-            raise InvalidCharacterError(f"Path cannot end with a dot ('.').")
-        return str_path
+            if character in strPath:
+                raise InvalidCharacterError(f"Invalid character '{character}' in '{strPath}'")
+        if ":" in strPath:
+            if not verInfo.pathRootHasColon:
+                raise InvalidCharacterError(f"Path has a colon but '{verInfo.os}' doesn't use colon for path root: '{strPath}'")
+            if strPath[1] != ":":
+                raise InvalidCharacterError(f"Path has a colon but there's no colon at index 1: '{strPath}'")
+            if strPath[2] != self.pathDelimiter:
+                raise InvalidCharacterError(f"Path has a colon but index 2 is not a delimiter: '{strPath}'")
+            if ":" in strPath[2:]:
+                raise InvalidCharacterError(f"Path has a colon that's not at index 1: '{strPath}'")
 
-    def _trim(self, str_path):
-        if not self.verInfo.pathRootIsDelimiter and str_path.startswith(self.path_delimiter):
-            str_path = str_path[1:]
-        if str_path.endswith(self.path_delimiter):
-            str_path = str_path[0:-1]
-        return str_path
+        if not VerInfo().pathRootIsDelimiter and strPath.startswith(self.pathDelimiter):
+            strPath = strPath[1:]
 
-setattr(Path, "Path", Path)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if strPath.endswith(self.pathDelimiter):
+            strPath = strPath[0:-1]
