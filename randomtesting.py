d6950c130ba77014403f6e7d0d1beedbc0c9a03b
Mandera <krizoorn@hotmail.com>
2020-10-03 00:18:10 +0200

    SSH interpreter testing commit

--- randomtesting.py
+++ randomtesting.py
@@ -1,8 +1,11 @@
 
 from generalfile import Path
 
-# print(Path("hi.txt").get_lock_dir())
+print(Path().absolute())
+# print(Path.get_lock_dir())
 
+# with open(str(Path.get_lock_dir() / "file.txt"), "w") as f:
+#     f.write("hello")
 
 
 
