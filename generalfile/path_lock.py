54e03d7b059065c6b5f71de965fc2122df0e48be
Mandera <krizoorn@hotmail.com>
2020-10-07 08:30:55 +0200

    Works great! Passing on linux vm and local W7
    Trying new method for lock where it uses Path cls to store locks.
    New branch as it's experimental.
    
    Found problem for failing thread test.
    
    Changed lock to W mode instead of X as linux didn't seem to be able to adopt it.
    Added a cache for working dir to fall back on if it's missing.
    Changed absolute to use our get_working_dir as Pathlib's absolute method failed the same way.
    
    Fixed leading path delimiter not being removed on posix, made lock work.

--- generalfile/path_lock.py
+++ generalfile/path_lock.py
@@ -10,50 +10,45 @@ class _Lock:
             Make sure only locked by self.
         A lock is inactive if it can be removed, as there's no Lock holding it's file stream.
         """
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, abs_path, *other_paths):
+        self.path = abs_path
         self.lock_file_stream = None
+        # TODO: other_paths
 
     def __enter__(self):
         self._attempt_lock_creation()
 
-        self.path.locked_paths.append(self.path)
-
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._close_and_remove_lock()
 
-        self.path.locked_paths.remove(self.path)
+    def _delete_if_dead(self, lock_path):
+        try:
+            seconds_since_creation = lock_path.seconds_since_modified()
+        except:
+            pass
+        else:
+            if seconds_since_creation > lock_path.dead_lock_seconds:
+                lock_path.delete(error=False)
 
     def _attempt_lock_creation(self):
-        path_absolute = self.path.absolute()
         timer = Timer()
         while timer.seconds() < self.path.timeout_seconds:
-
             affecting_locks = list(self._affecting_locks())
             if self._is_locked_externally(affecting_locks=affecting_locks):
                 for locked_path in affecting_locks:
-                    lock_path = locked_path.get_lock_path()
-                    try:
-                        seconds_since_creation = lock_path.seconds_since_modified()
-                    except:
-                        pass
-                    else:
-                        if seconds_since_creation > lock_path.dead_lock_seconds:
-                            lock_path.delete()
+                    self._delete_if_dead(locked_path.get_lock_path())
             else:
                 if not self._open_and_create_lock():
                     continue  # If lock failed creating
 
                 affecting_locks = list(self._affecting_locks())
-                if path_absolute in affecting_locks:
+                if self.path in affecting_locks:
                     if self._is_locked_externally(affecting_locks=affecting_locks):
                         self._close_and_remove_lock()  # Remove and try again to respect other locks
                     else:
-                        return
+                        return  # Success
                 else:
                     raise FileNotFoundError(f"Lock '{self.path}' failed to create.")
-            # else:
-                # print(path_absolute, list(self._affecting_locks()))
         raise TimeoutError(f"Couldn't lock '{self.path}' in time.")
 
     def _open_and_create_lock(self):
@@ -66,25 +61,30 @@ class _Lock:
             return False
 
         self.lock_file_stream.write("foo")
+
+        self.path.locked_paths.append(self.path)
         return True
 
     def _close_and_remove_lock(self):
         if self.lock_file_stream is None:
             raise AttributeError(f"A file stream is not opened for '{self.path}'.")
 
+        self.path.locked_paths.remove(self.path)
+
         self.lock_file_stream.close()
+        self.lock_file_stream = None
+
         self.path.get_lock_path().delete(error=False)
 
     def _affecting_locks(self):
         """ Returns absolute paths in list pointing to path it's locking.
-            Use `get_alternative_path()` if actual lock path is wanted.
+            Use `get_alternative_path()` or 'get_lock_path()' if actual lock path is wanted.
 
             :rtype: tuple[generalfile.Path] """
-        path_absolute = self.path.absolute()
         for alternative_path in self.path.get_lock_dir().get_paths_in_folder():
 
             path = alternative_path.get_path_from_alternative()
-            if path_absolute.startswith(path) or path.startswith(path_absolute):
+            if self.path.startswith(path) or path.startswith(self.path):
                 yield path
 
     def _is_locked_externally(self, affecting_locks=None):
@@ -113,16 +113,43 @@ class Path_ContextManager:
         else:
             return _Lock(path, *other_paths)  # Create real lock
 
-    def lock(self, *other_paths):  # HERE ** remove args
+    def lock(self, *other_paths):
         """ Create a lock for this path unless path is inside `lock dir`.
-            Optionally supply additional paths to prevent them from interfering as well as creating locks for them too.
+            Optionally supply additional paths to create locks for them too.
 
             :param generalfile.Path self: """
-        other_paths = list(other_paths)
-        for path in other_paths:
-            paths_list = other_paths.copy()
-            paths_list[paths_list.index(path)] = self  # Replace ´path´ with self
-            self._create_context_manager(path, *paths_list)
+        return self._create_context_manager(self.absolute(), *other_paths)
 
-        return self._create_context_manager(self, *other_paths)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- randomtesting.py
+++ randomtesting.py
@@ -6,27 +6,11 @@ from generalfile.test.setup_workdir import setup_workdir
 
 
 # setup_workdir()
-# Path("hello.txt").write("asdf")
-
-# print(Path("hello.txt").seconds_since_modified())
 
 
 
-# with Path("hello").lock():
-#     with Path("hello").lock():
-#         print(5)
 
 
-
-a = open("test.txt", "x")
-a.write("hello")
-
-b = open("test.txt", "r")
-
-print(b.read())
-
-a.close()
-b.close()
 
 # TODO: Add locked_paths to Path cls, remove _is_locked_externally, remove owns_lock, remove *args for lock()
 # TODO: Doesn't seem to able to remove dead locks
