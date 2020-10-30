9f86e584057a5793433eface9df4023deb939f0e
Mandera <krizoorn@hotmail.com>
2020-10-30 22:57:56 +0100

    Fixed trailing comma in json

--- randomtesting.py
+++ randomtesting.py
@@ -13,3 +13,24 @@ from generalfile import Path
 # getLocalFeaturesAsMD(locals(), "generalfile")
 # getLocalFeaturesAsMD({k: getattr(Path, k, None) for k in dir(Path)}, "generalfile")
 
+import configparser
+import json
+
+
+class Cfg:  # I think we could actually use generalfile here instead, as we are doing that in lib already -> Add cfg as option dependency
+    def __init__(self, path):
+        self.config = configparser.RawConfigParser()
+        self.config.read(path)
+
+    def __call__(self, section, option):
+        result = self.config.get(section, option)
+        try:
+            return json.loads(result)
+        except json.decoder.JSONDecodeError:
+            return result
+
+
+cfg = Cfg("package_specific.cfg")
+
+
+print(cfg("setup", "extras_require"))
