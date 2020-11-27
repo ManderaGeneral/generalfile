b20692418af39cdfd8f72638072d8c6e5de2a52b
Mandera <krizoorn@hotmail.com>
2020-11-27 16:37:02 +0100

    Fixed cfg reading for nested iterables.

--- randomtesting.py
+++ randomtesting.py
@@ -1,18 +1,10 @@
 
 
 
-from generalfile import Path
-from generalfile.test.setup_workdir import setup_workdir
-setup_workdir()
-Path.get_working_dir().open_folder()
+# from generalfile import Path
+# from generalfile.test.setup_workdir import setup_workdir
+# setup_workdir()
+# Path.get_working_dir().open_folder()
 # Path.get_lock_dir().open_folder()
 
 
-import json
-
-
-# print(json.loads('[1, 2, "a"]'))
-
-
-Path("foo").cfg.write({'test': {'foo': 'bar', "hi": ["a", "b", 3]}})
-print(Path("foo").cfg.read())
