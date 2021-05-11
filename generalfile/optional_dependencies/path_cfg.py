f55483a09604abf5c7a9c49656caf804125c0da9
Mandera <rickard.abraham@gmail.com>
2021-05-11 10:28:13 +0200

    Refactored optional dependency class creation.

--- generalfile/optional_dependencies/path_cfg.py
+++ generalfile/optional_dependencies/path_cfg.py
@@ -1,8 +1,9 @@
 
 from generallibrary import deco_cache, initBases
+from generalfile.optional_dependencies._extension import _Extension
+
 import configparser
 import json
-
 
 
 class Path_Cfg:
@@ -10,45 +11,40 @@ class Path_Cfg:
     @property
     @deco_cache()
     def cfg(self):
-        """ Easily read and write to cfg files with a dictionary.
-            Easy access to a dynamically one-time created Cfg class, initalized with self (Path). """
-        return self._cfg()(self)
-
-    @staticmethod
-    @deco_cache()
-    def _cfg():
-        @initBases
-        class _Cfg(_Extension):
-            def write(self, dict_=None, overwrite=False):
-                """ Write to this path with a given dictionary. """
-                config = configparser.RawConfigParser()
-                config.read_dict(dictionary=dict_)
-                with self.WriteContext(self.path, overwrite=overwrite) as write_path:
-                    return write_path.open_operation("w", lambda stream: config.write(stream))
-
-            def _read_json_cast(self, value):
-                try:
-                    return json.loads(value.replace("'", '"'))
-                except json.decoder.JSONDecodeError:
-                    return value
-
-            def read(self):
-                """ Read from this path to get a dictionary. """
-                config = configparser.RawConfigParser()
-                with self.ReadContext(self.path) as read_path:
-                    config.read(read_path)
-                return {s: {key: self._read_json_cast(value) for key, value in config.items(s)} for s in config.sections()}
-
-            def append(self, dict_):
-                """ Update this cfg with a dictionary. """
-                with self.AppendContext(self.path) as append_path:
-                    # append_path.cfg.write(dict_=append_path.cfg.read() | dict_, overwrite=True)
-                    append_path.cfg.write(dict_={**append_path.cfg.read(), **dict_}, overwrite=True)
-
-        return _Cfg
+        """ Easily modify cfg files with a dictionary. """
+        return _Cfg(self)
 
 
-from generalfile.path_operations import _Extension
+@initBases
+class _Cfg(_Extension):
+    def write(self, dict_=None, overwrite=False):
+        """ Write to this path with a given dictionary. """
+        config = configparser.RawConfigParser()
+        config.read_dict(dictionary=dict_)
+        with self.WriteContext(self.path, overwrite=overwrite) as write_path:
+            return write_path.open_operation("w", lambda stream: config.write(stream))
+
+    def _read_json_cast(self, value):
+        try:
+            return json.loads(value.replace("'", '"'))
+        except json.decoder.JSONDecodeError:
+            return value
+
+    def read(self):
+        """ Read from this path to get a dictionary. """
+        config = configparser.RawConfigParser()
+        with self.ReadContext(self.path) as read_path:
+            config.read(read_path)
+        return {s: {key: self._read_json_cast(value) for key, value in config.items(s)} for s in config.sections()}
+
+    def append(self, dict_):
+        """ Update this cfg with a dictionary. """
+        with self.AppendContext(self.path) as append_path:
+            # append_path.cfg.write(dict_=append_path.cfg.read() | dict_, overwrite=True)
+            append_path.cfg.write(dict_={**append_path.cfg.read(), **dict_}, overwrite=True)
+
+
+
 
 
 
