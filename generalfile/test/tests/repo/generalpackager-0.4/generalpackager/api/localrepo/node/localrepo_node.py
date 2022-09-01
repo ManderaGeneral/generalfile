0cc597ca3665147064c7bca72b91fa7cac7df315
runner <runner@fv-az96-284>
2022-09-01 14:51:09 +0000

    [CI AUTO] Sync triggered by generalpackager

--- generalfile/test/tests/repo/generalpackager-0.4/generalpackager/api/localrepo/node/localrepo_node.py
+++
@@ -1,9 +0,0 @@
-
-from generalpackager.api.localrepo.base.localrepo import LocalRepo
-from generalpackager.api.localrepo.node.metadata_node import Metadata_Node
-
-
-class LocalRepo_Node(LocalRepo):
-    cls_target = LocalRepo.Targets.node
-    cls_metadata = Metadata_Node
-
