c1ab74dc04aa7c919fb54120270027bf0e02aada
Mandera <rickard.abraham@gmail.com>
2023-02-15 16:21:08 +0700

    Disallow high level op on tmp file.
    Also made _Context dry

--- generalfile/optional_dependencies/_extension.py
+++ generalfile/optional_dependencies/_extension.py
@@ -1,19 +1,34 @@
 
 
+
 class _Context:
     def __init__(self, path):
         self.path = path
-        self.temp_path = self.path.with_suffix(".tmp")
         self.lock = self.path.lock()
 
     def __enter__(self):
         self.lock.__enter__()
+        return self.path
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.lock.__exit__(exc_type, exc_val, exc_tb)
 
 
-class WriteContext(_Context):
+class _SharedWriteAppend(_Context):
+    SUFFIX = ".tmp"
+
+    def __init__(self, path):
+        super().__init__(path)
+        assert path.suffix() != self.SUFFIX, f"High level operation on {self.SUFFIX} isn't allowed. Use open_operation in most cases."
+        self.temp_path = self.path.with_suffix(self.SUFFIX)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.temp_path.exists():
+            self.temp_path.rename(self.path, overwrite=True)
+        super().__exit__(exc_type, exc_val, exc_tb)
+
+
+class WriteContext(_SharedWriteAppend):
     """ A context manager used for every write method. """
     def __init__(self, path, overwrite=False):
         super().__init__(path)
@@ -27,38 +42,21 @@ class WriteContext(_Context):
         self.path.get_parent().create_folder()
         return self.temp_path
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.temp_path.rename(self.path, overwrite=True)
-        super().__exit__(exc_type, exc_val, exc_tb)
 
-
-class ReadContext(_Context):
-    """ A context manager used for every read method. """
-    def __init__(self, path):
-        super().__init__(path)
-
-    def __enter__(self):
-        super().__enter__()
-        return self.path
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        super().__exit__(exc_type, exc_val, exc_tb)
-
-
-class AppendContext(_Context):
+class AppendContext(_SharedWriteAppend):
     """ A context manager used for every append method. """
-    def __init__(self, path):
-        super().__init__(path)
-
     def __enter__(self):
         super().__enter__()
         if self.path.exists():
             self.path.copy(self.temp_path)
+        else:
+            self.path.get_parent().create_folder()
         return self.temp_path
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.temp_path.rename(self.path, overwrite=True)
-        super().__exit__(exc_type, exc_val, exc_tb)
+
+class ReadContext(_Context):
+    """ A context manager used for every read method. """
+
 
 
 class _Extension:
