ec22866b50ab90e4e22be8db1b44de1836011763
Mandera <rickard.abraham@gmail.com>
2023-02-15 16:50:17 +0700

    Testing hooks

--- randomtesting.py
+++ randomtesting.py
@@ -1,13 +1,17 @@
 
 from generalfile import *
+
+from generalpackager import Venv
+
+print(Venv.get_active_venv())
 from generalfile.test.setup_workdir import setup_workdir
 
 from generallibrary import Ver
 
-# Path("hi").write()
+Path("hi").write()
 
-with Path("hi").as_renamed("hello") as path:
-    print(path, path.exists())
+# with Path("hi").as_renamed("hello") as path:
+#     print(path, path.exists())
 
 
 # print((Path("hi").as_working_dir(context=False)))
